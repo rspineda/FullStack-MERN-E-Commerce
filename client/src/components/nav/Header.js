99bbdaecfa4cdf88661b4ff187d713c92b87a272
Ronald <ronystivens@hotmail.com>
2021-01-07 00:45:59 +0100

    added style and sections to navigation menu

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,25 +1,32 @@
 import React, {useState} from 'react';
 import {Menu} from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import { AppstoreOutlined, SettingOutlined, UserOutlined, UserAddOutlined } from '@ant-design/icons';
 
-const { SubMenu } = Menu;
+const { SubMenu, Item } = Menu;
 
 const Header = () => {
 
-    const [current, setCurrent] = useState('');
-    const handleClick = () => {
-        //
+    const [current, setCurrent] = useState('home');
+    const handleClick = (e) => {
+        //console.log(e.key);
+        setCurrent(e.key);
     }
 
     return (
 
       <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-        <Menu.Item key="mail" icon={<MailOutlined />}>
+        <Item key="home" icon={<AppstoreOutlined />}>
           Home
-        </Menu.Item>   
-        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Register">
-          <Menu.Item key="setting:1">Option 1</Menu.Item>
-          <Menu.Item key="setting:2">Option 2</Menu.Item>         
+        </Item>
+        <Item key="register" icon={<UserAddOutlined />} className="float-right">
+          Register
+        </Item>  
+        <Item key="login" icon={<UserOutlined />} className="float-right">
+          Login
+        </Item>      
+        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
+          <Item key="setting:1">Option 1</Item>
+          <Item key="setting:2">Option 2</Item>         
         </SubMenu>      
       </Menu>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,11 @@ import { BrowserRouter } from 'react-router-dom';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </BrowserRouter>,
+  //</React.StrictMode>,
   document.getElementById('root')
 );
 
