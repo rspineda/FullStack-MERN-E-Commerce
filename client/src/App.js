eb073e4a11bf440574155ea163f7c75eb6609b7b
Ronald <ronystivens@hotmail.com>
2021-01-06 13:19:41 +0100

    added navigation menu

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,19 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/Home';
+import Header from './components/nav/Header';
 
 const App = () => {
   return (
+    <>
+    <Header></Header>
     <Switch>
       <Route exact path="/" component={Home}></Route>
       <Route exact path="/login" component={Login}></Route>
       <Route exact path="/register" component={Register}></Route>
     </Switch>
+    </>
+    
   )
 }
 
