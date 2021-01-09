5660100ca96364a297777dadfe7e926bbbe093e8
Ronald <ronystivens@hotmail.com>
2021-01-09 23:39:25 +0100

    added register form

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 const Register = () => {
+
+    const [email, setEmail] = useState('');
+    const handleSubmit = () => {
+
+    }
+    const registerForm = () => (
+    
+        <form onSubmit={handleSubmit}>
+            <input type="email" className="form-control" value={email} onChange={(e)=> setEmail(e.target.value)} autoFocus></input>
+            <button type="submit" className="btn btn-raised">email</button>
+        </form>
+    );
+
     return (
-        <div>
-            <p>Home</p>
+        <div className="container p-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h4>Register</h4>
+                    {registerForm()}
+                </div>
+            </div>
         </div>
     );
 };
