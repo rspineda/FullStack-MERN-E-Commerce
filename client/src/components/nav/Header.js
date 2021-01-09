2dda9aec13239327fd305dc4b2eeeeb1d3f768ea
Ronald <ronystivens@hotmail.com>
2021-01-09 22:52:11 +0100

    added home, and login and register routes

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {Menu} from 'antd';
 import { AppstoreOutlined, SettingOutlined, UserOutlined, UserAddOutlined } from '@ant-design/icons';
+import {Link} from 'react-router-dom';
 
 const { SubMenu, Item } = Menu;
 
@@ -16,13 +17,13 @@ const Header = () => {
 
       <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
         <Item key="home" icon={<AppstoreOutlined />}>
-          Home
+          <Link to="/">Home</Link>
         </Item>
         <Item key="register" icon={<UserAddOutlined />} className="float-right">
-          Register
+          <Link to="/register">Register</Link>
         </Item>  
         <Item key="login" icon={<UserOutlined />} className="float-right">
-          Login
+          <Link to="/login">Login</Link>
         </Item>      
         <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
           <Item key="setting:1">Option 1</Item>
