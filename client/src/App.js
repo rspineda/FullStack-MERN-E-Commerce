f19b5d9dc9fcc2e417ab94aaa487ccdcacc3c56c
Ronald <ronystivens@hotmail.com>
2021-01-11 23:40:36 +0100

    handled client page for setting the user-password

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/Home';
 import Header from './components/nav/Header';
+import RegisterComplete from './pages/auth/RegisterComplete';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <Route exact path="/" component={Home}></Route>
       <Route exact path="/login" component={Login}></Route>
       <Route exact path="/register" component={Register}></Route>
+      <Route exact path="/register/complete" component={RegisterComplete}></Route>
     </Switch>
     </>
     
