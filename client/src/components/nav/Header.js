b1442d20277c24bfd83fff55c4946cfa2eba963b
Ronald <ronystivens@hotmail.com>
2021-01-13 23:34:20 +0100

    added logout functionality

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -2,15 +2,30 @@ import React, {useState} from 'react';
 import {Menu} from 'antd';
 import { AppstoreOutlined, SettingOutlined, UserOutlined, UserAddOutlined } from '@ant-design/icons';
 import {Link} from 'react-router-dom';
+import firebase from 'firebase';
+import {useDispatch} from 'react-redux';
+import {useHistory} from 'react-router-dom';
 
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
 
     const [current, setCurrent] = useState('home');
+    let dispatch = useDispatch();
+    let history = useHistory();
+
     const handleClick = (e) => {
         //console.log(e.key);
         setCurrent(e.key);
+    }
+
+    const logout = () => {
+      firebase.auth().signOut();
+      dispatch({
+        type: "LOGOUT",
+        payload: null
+      });
+      history.push('/login');
     }
 
     return (
@@ -27,7 +42,8 @@ const Header = () => {
         </Item>      
         <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
           <Item key="setting:1">Option 1</Item>
-          <Item key="setting:2">Option 2</Item>         
+          <Item key="setting:2">Option 2</Item>
+          <Item icon={<UserOutlined></UserOutlined>} onClick={logout}>Logout</Item>          
         </SubMenu>      
       </Menu>
     );
