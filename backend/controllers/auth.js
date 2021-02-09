628ae00fc707358acf27beac436a791958b3cbf7
Ronald <ronystivens@hotmail.com>
2021-02-09 11:46:06 +0100

    moved controllers to a different folder

---
+++ backend/controllers/auth.js
@@ -0,0 +1,6 @@
+
+exports.createOrUpdateUser = (req,res) => {
+    res.json({
+        data: "Hey Ronald, welcome to the create-or-update-user API endpoint"
+    })
+}
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/create-or-update-user', (req,res) => {
-    res.json({
-        data: "Hey Ronald, welcome to the create-or-update-user API endpoint"
-    })
-});
+//import
+const { createOrUpdateUser } = require('../controllers/auth');
+
+router.get('/create-or-update-user', createOrUpdateUser);
 
 module.exports = router;
