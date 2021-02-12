ef880753867e1b3967ad4d5bb8e1e065a95296c1
Ronald <ronystivens@hotmail.com>
2021-02-12 10:31:39 +0100

    added middlewares structure

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,5 @@
+const admin = require('../firebase/index');
+
+exports.authCheck = (req, res, next) => {
+    console.log(req.headers); //client token
+};
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+//midlewares
+const { authChech } = require('../middlewares/auth');
+
 //import
 const { createOrUpdateUser } = require('../controllers/auth');
 
-router.get('/create-or-update-user', createOrUpdateUser);
+router.post('/create-or-update-user', authChech ,createOrUpdateUser);
 
 module.exports = router;
