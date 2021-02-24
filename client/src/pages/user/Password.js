8a54b1457143ae45e32df6137eac910823aeb1b0
Ronald <ronystivens@hotmail.com>
2021-02-24 23:59:36 +0100

    setting how to update user password

--- client/src/pages/user/Password.js
+++ client/src/pages/user/Password.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserNav from '../../components/nav/UserNav';
+import { auth } from '../../firebase';
+import { toast } from 'react-toastify';
 
-const Password = () => (
+const Password = () => {
+
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(password);
+    }
+
+    const passwordUpdateForm = () => (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label>Your Password</label>
+                <input type="password" onChange={(e)=>setPassword(e.target.value)} className="form-control" placeholder="Enter new password"></input>
+                <button className="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    );
+
+    return (
     <div className="container-fluid">
         <div className="row">
             <div className="col-md-2">
                 <UserNav></UserNav>
             </div>
-            <div className="col">user password update page</div>
+            <div className="col">
+                <h4>Password Update</h4>
+                {passwordUpdateForm()}
+            </div>
         </div>
     </div>
-);
+    )
+};
 
 export default Password;
