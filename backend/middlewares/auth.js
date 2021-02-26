10c8b10b65f70d390a3b31dc55bc59809a48c685
Ronald <ronystivens@hotmail.com>
2021-02-26 23:03:58 +0100

    added an admin middleware in order to secure the access to admin page

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,5 +1,7 @@
 const admin = require('../firebase/index');
+const User = require('../models/user');
 
+//Check if the user is authenticated
 exports.authCheck = async (req, res, next) => {
     //console.log(req.headers); //client token
     try{
@@ -11,5 +13,20 @@ exports.authCheck = async (req, res, next) => {
         res.status(401).json({
             err: "Invalid or expired token"
         });
+    }
+};
+
+//Check if the authenticated  user is admin, so allow him to next pages
+exports.adminCheck = async (req, res, next) => {
+    const { email } = req.user;
+
+    const adminUser = await (await User.findOne({ email: email})).exec();
+
+    if(adminUser.role !== 'admin') {
+        res.status(403).json({
+          err: 'Admin resource. Acces denied'  
+        });
+    }else{
+        next();
     }
 };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 //midlewares
-const { authCheck } = require('../middlewares/auth');
+const { authCheck, adminCheck } = require('../middlewares/auth');
 
 //controller
 const { createOrUpdateUser, currentUser } = require('../controllers/auth');
 
 router.post('/create-or-update-user', authCheck, createOrUpdateUser);
 router.post('/current-user', authCheck, currentUser);
+router.post('/current-admin', authCheck, adminCheck, currentUser);
 
 module.exports = router;
