e1b0bc478f6332685f4127124514d597a822227e
Ronald <ronystivens@hotmail.com>
2021-02-28 10:55:59 +0100

    handled create category endpoint

---
+++ backend/controllers/category.js
@@ -0,0 +1,28 @@
+const Category = require('../models/category');
+const slugify = require('slugify');
+
+exports.create = async (req, res) => {
+    try{
+        const { name } = req.body;
+        const category = await new Category({ name: name , slug: slugify(name)}).save();
+        res.json(category);
+    }catch(err){
+        res.status(400).send('Create category failed');
+    }
+};
+
+exports.list = async (req, res) => {
+
+};
+
+exports.read = async (req, res) => {
+
+};
+
+exports.update = async (req, res) => {
+
+};
+
+exports.remove = async (req, res) => {
+
+};
