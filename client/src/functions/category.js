fa4690df0be66e1cfaf2d75f94dfe86c199ec64c
Ronald <ronystivens@hotmail.com>
2021-03-02 00:28:00 +0100

    added all crud entries (on the client side) for categories

--- client/src/functions/category.js
+++ client/src/functions/category.js
@@ -1,6 +1,36 @@
 import axios from 'axios';
 
-//get all categories
+//CRUD entries (on client) for categories:
+
 export const getCategories= async () => {
-    return await  axios.post(`${process.env.REACT_APP_API}/categories`);
+    return await  axios.get(`${process.env.REACT_APP_API}/categories`);
+};
+
+
+export const getCategory= async (slug) => {
+    return await  axios.get(`${process.env.REACT_APP_API}/category/${slug}`);
+};
+
+export const removeCategory= async (slug, authtoken) => {
+    return await  axios.delete(`${process.env.REACT_APP_API}/category/${slug}`, {
+        headers: {
+            authtoken: authtoken
+        }
+    });
+};
+
+export const updateCategory= async (slug, category, authtoken) => {
+    return await  axios.put(`${process.env.REACT_APP_API}/category/${slug}`, {
+        headers: {
+            authtoken: authtoken
+        }
+    });
+};
+
+export const createCategory= async (category, authtoken) => {
+    return await  axios.post(`${process.env.REACT_APP_API}/category`, category, {
+        headers: {
+            authtoken: authtoken
+        }
+    });
 };
