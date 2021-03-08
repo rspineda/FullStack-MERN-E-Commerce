cdec0575c66da8558ab3c42ca8cb591c42b2c530
Ronald <ronystivens@hotmail.com>
2021-03-08 22:12:17 +0100

    added CategoryCreate page on Admin Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import AdminRoute from './components/routes/AdminRoute';
 import Password from './pages/user/Password';
 import Wishlist from './pages/user/Wishlist';
 import AdminDashboard from './pages/admin/AdminDashboard';
+import CategoryCreate from './pages/admin/category/CategoryCreate';
 
 import {auth} from './firebase';
 import {useDispatch} from 'react-redux';
@@ -66,6 +67,7 @@ const App = () => {
       <UserRoute exact path="/user/password" component={Password}></UserRoute>
       <UserRoute exact path="/user/wishlist" component={Wishlist}></UserRoute>
       <AdminRoute exact path="/admin/dashboard" component={AdminDashboard}></AdminRoute>
+      <AdminRoute exact path="/admin/category" component={CategoryCreate}></AdminRoute>
     </Switch>
     </>
     
---
+++ client/src/pages/admin/category/CategoryCreate.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AdminNav from '../../../components/nav/AdminNav';
+
+const CategoryCreate = () => {
+    return (
+        <div className="container-fluid">
+        <div className="row">
+            <div className="col-md-2">
+                <AdminNav></AdminNav>
+            </div>
+            <div className="col">Category Create Page</div>
+        </div>
+    </div>
+    );
+};
+
+export default CategoryCreate;
