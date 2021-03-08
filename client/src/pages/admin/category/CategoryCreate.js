695b2f85491dba825577f12dd57f1487d3075828
Ronald <ronystivens@hotmail.com>
2021-03-08 23:48:17 +0100

    handled create category endpoint from admin frontend request

--- client/src/pages/admin/category/CategoryCreate.js
+++ client/src/pages/admin/category/CategoryCreate.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AdminNav from '../../../components/nav/AdminNav';
+import { toast } from 'react-toastify';
+import { useSelector } from 'react-redux';
+import { createCategory, getCategory, removeCategory } from '../../../functions/category';
 
 const CategoryCreate = () => {
+
+    const { user } = useSelector((state) => ({...state}));
+    const [name, setName] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //console.log(name)
+        setLoading(true);
+
+        createCategory({name:name}, user.token)
+        .then((res) => {
+            //console.log(res);
+            setLoading(false);
+            setName('');
+            toast.success(`"${res.data.name}" was created`);
+        })
+        .catch(err => {
+            //console.log(err)
+            setLoading(false);
+            if (err.response.status === 400) toast.error(err.response.data);
+        })
+    }
+
+    const categoryForm = () => (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label>Name</label>
+                <input type="text" className="form-control" onChange={(e) => setName(e.target.value)} value={name} autoFocus required></input>
+                <br></br>
+                <button className="btn btn-outline-primary">Save</button>
+            </div>
+        </form>
+    );
+
     return (
         <div className="container-fluid">
         <div className="row">
             <div className="col-md-2">
                 <AdminNav></AdminNav>
             </div>
-            <div className="col">Category Create Page</div>
+            <div className="col">
+                <h4>Create Category </h4>
+                {categoryForm()}
+            </div>
         </div>
     </div>
     );
