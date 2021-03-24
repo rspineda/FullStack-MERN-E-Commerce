1024a110037980610a7d8cc33610880cd1aaf3c4
Ronald <ronystivens@hotmail.com>
2021-03-24 23:44:04 +0100

    added subcategory route on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import Wishlist from './pages/user/Wishlist';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import CategoryCreate from './pages/admin/category/CategoryCreate';
 import CategoryUpdate from './pages/admin/category/CategoryUpdate';
+import SubCreate from './pages/admin/sub/SubCreate';
 
 import {auth} from './firebase';
 import {useDispatch} from 'react-redux';
@@ -70,6 +71,7 @@ const App = () => {
       <AdminRoute exact path="/admin/dashboard" component={AdminDashboard}></AdminRoute>
       <AdminRoute exact path="/admin/category" component={CategoryCreate}></AdminRoute>
       <AdminRoute exact path="/admin/category/:slug" component={CategoryUpdate}></AdminRoute>
+      <AdminRoute exact path="/admin/sub" component={SubCreate}></AdminRoute>
     </Switch>
     </>
     
---
+++ client/src/pages/admin/sub/SubCreate.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AdminNav from '../../../components/nav/AdminNav';
+
+const SubCreate = () => {
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-2">
+                    <AdminNav></AdminNav>
+                </div>
+                <div className="col-md-10">
+                sub create page
+                </div>   
+            </div>       
+        </div>
+    )
+};
+
+export default SubCreate;
