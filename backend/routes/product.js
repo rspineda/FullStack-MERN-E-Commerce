c5a10562bf37dcb7f88f2058c136340cf5cc2f58
Ronald <ronystivens@hotmail.com>
2021-03-31 22:49:39 +0200

    handled create product route/controller on backend

---
+++ backend/routes/product.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+//midlewares
+const { authCheck, adminCheck } = require('../middlewares/auth');
+
+//controller
+const { create, read, update, remove, list } = require('../controllers/product');
+
+//routes
+router.post('/product', authCheck, adminCheck, create);
+
+//similar to categories:
+//router.get('/categories', list);
+//router.get('/category/:slug', read);
+//router.put('/category/:slug', authCheck, adminCheck, update);
+//router.delete('/category/:slug', authCheck, adminCheck, remove);
+
+
+module.exports = router;
