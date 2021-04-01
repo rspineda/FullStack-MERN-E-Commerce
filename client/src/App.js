4f8ba9ed3fa99c45a8f8b671f6bca7e30765105e
Ronald <ronystivens@hotmail.com>
2021-04-01 23:25:51 +0200

    handled create product page-structure/function on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import CategoryCreate from './pages/admin/category/CategoryCreate';
 import CategoryUpdate from './pages/admin/category/CategoryUpdate';
 import SubCreate from './pages/admin/sub/SubCreate';
 import SubUpdate from './pages/admin/sub/SubUpdate';
+import ProductCreate from './pages/admin/product/ProductCreate';
 
 import {auth} from './firebase';
 import {useDispatch} from 'react-redux';
@@ -74,6 +75,7 @@ const App = () => {
       <AdminRoute exact path="/admin/category/:slug" component={CategoryUpdate}></AdminRoute>
       <AdminRoute exact path="/admin/sub" component={SubCreate}></AdminRoute>
       <AdminRoute exact path="/admin/sub/:slug" component={SubUpdate}></AdminRoute>
+      <AdminRoute exact path="/admin/product" component={ProductCreate}></AdminRoute>
     </Switch>
     </>
     
