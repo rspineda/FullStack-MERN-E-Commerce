daca2a659a2d6f6207569b1e1c5dd4b6919c0794
Ronald <ronystivens@hotmail.com>
2021-04-10 23:46:15 +0200

    handled subcategories request attached to a parent category, on backend

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -1,4 +1,5 @@
 const Category = require('../models/category');
+const Sub = require('../models/sub');
 const slugify = require('slugify');
 
 exports.create = async (req, res) => {
@@ -42,3 +43,10 @@ exports.remove = async (req, res) => {
         res.status(400).send("Delete category failed");
     }
 };
+
+exports.getSubs = (req, res) => {
+    Sub.find({parent: req.params._id}).exec((err,subs)=>{
+        if(err) console.log(err);
+        res.json(subs);
+    });
+}
--- backend/routes/category.js
+++ backend/routes/category.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { authCheck, adminCheck } = require('../middlewares/auth');
 
 //controller
-const { create, read, update, remove, list } = require('../controllers/category');
+const { create, read, update, remove, list, getSubs } = require('../controllers/category');
 
 //routes
 router.post('/category', authCheck, adminCheck, create);
@@ -13,6 +13,6 @@ router.get('/categories', list);
 router.get('/category/:slug', read);
 router.put('/category/:slug', authCheck, adminCheck, update);
 router.delete('/category/:slug', authCheck, adminCheck, remove);
-
+router.get('/category/subs/:id', getSubs); //it will return the subcategories attached to a specific parent category.
 
 module.exports = router;
