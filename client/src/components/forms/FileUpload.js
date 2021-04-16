3d60d224b672e733b372786f277f99e5329ce753
Ronald <ronystivens@hotmail.com>
2021-04-17 00:10:21 +0200

    resized product images on frontend using react-image-file-resizer

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -1,21 +1,39 @@
 import React from 'react';
+import Resizer from 'react-image-file-resizer';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 const FileUpload = () => {
+    const { user } = useSelector((state) => ({...state}));
 
-    const fileUploadAndResize = () => {
-        //
+    const fileUploadAndResize = (e) => {
+        //console.log(e.target.files);
+        //resize:
+        let files = e.target.files;
+        if(files) {
+            for (let i=0; i<files.length; i++) {
+                Resizer.imageFileResizer(files[i], 720, 720, 'JEPG', 100, 0, (uri) => {
+                    console.log(uri)
+                }, 'base64');
+            }
+        }
+        //send back to server to upload to cloudinary
+        //set url to images[] in the parent component - ProductCreate
     };
 
     return (
         <div className="row">
-            <label>Choose File</label>
-            <input 
-                type="file" 
-                multiple 
-                accept="images/*" 
-                onChange={fileUploadAndResize}
-                >
-            </input>
+            <label className="btn btn-primary btn-raised">
+                Choose File
+                <input 
+                    type="file" 
+                    multiple
+                    hidden 
+                    accept="images/*" 
+                    onChange={fileUploadAndResize}
+                    >
+                </input>
+            </label>
         </div>
     );
 }
