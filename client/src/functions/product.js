06786063a057f36a86f1c9ef8363dc6e383463fd
Ronald <ronystivens@hotmail.com>
2021-05-01 00:40:07 +0200

    fetching all products to the admin dashboard on frontend

--- client/src/functions/product.js
+++ client/src/functions/product.js
@@ -7,3 +7,8 @@ export const createProduct = async (product, authtoken) => {
         }
     });
 };
+
+export const getProductsByCount = async (count) => {
+   return await axios.get(`${process.env.REACT_APP_API}/products/${count}`);
+}
+
--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AdminNav from '../../components/nav/AdminNav';
+import { getProductsByCount } from '../../functions/product';
 
 const AdminDashboard = () => {
+
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        getProductsByCount(10)
+        .then((res) => setProducts(res.data))
+        .catch((err) => console.log(err));
+    }, []);
+
     return (
         <div className="container-fluid">
         <div className="row">
             <div className="col-md-2">
                 <AdminNav></AdminNav>
             </div>
-            <div className="col">Admin Dashboard Page</div>
+            <div className="col">{JSON.stringify(products)}</div>
         </div>
     </div>
     );
