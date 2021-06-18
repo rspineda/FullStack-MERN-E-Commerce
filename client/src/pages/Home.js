7bde32afcf9b92a311391da8dd426ebbf886b3f0
Ronald <ronystivens@hotmail.com>
2021-06-19 00:12:02 +0200

    rendered all products on Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { getProductsByCount } from '../functions/product';
+import ProductCard from "../components/cards/ProductCard";
 
-const Home = () => (
+const Home = () => {
   
-    <div>
-      <p>react home</p>
-    </div>
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-);
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
+  const loadAllProducts = () => {
+    setLoading(true);
+    getProductsByCount(3)
+    .then((res) => {
+      setProducts(res.data);
+      setLoading(false);
+    })
+  };
+
+  return (
+    <>
+    <div className="jumbotron">
+      {loading ? <h3>Loading...</h3> : <h3>All Products</h3>}
+    </div>
+    <div className="container">
+      <div className="row">
+        {products.map((product) => (
+          <div key={product._id} className="col-md-4">
+            <ProductCard
+              product={product}
+            />
+          </div>
+        ))}
+      </div>
+
+    </div>
+    </>
+  );
+};
 
 export default Home;
