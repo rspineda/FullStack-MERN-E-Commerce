f2ab5f89cfbe98aba760da1ae103d401c99a56c1
Ronald <ronystivens@hotmail.com>
2021-06-25 00:11:19 +0200

    render cards on home page where user can see all products displayed on those cards

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -1,5 +1,45 @@
 import React from 'react';
+import {Card} from 'antd';
+import { EyeOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import laptopImage from '../../images/laptop.jpg';
+import {Link} from 'react-router-dom';
 
-const ProductCard = ({product}) => <div>{product.title}</div>;
+const {Meta} = Card;
+
+const ProductCard = ({product}) => {
+
+  const {images, title, description, slug} = product;
+
+  return (
+      <Card
+        cover={
+            <img 
+                src={images && images.length ? images[0].url : laptopImage} 
+                style={{height:'150px', objectFit:'cover'}}
+                className='p-1'
+            />}
+        actions={[
+          <Link to={`product/${slug}`}>
+              <EyeOutlined className="text-warning"/>
+              <br/>
+              View Product
+          </Link>, 
+          <>
+            <ShoppingCartOutlined 
+                className="text-warning" 
+            />
+            <br/>
+            Add to Cart
+          </>,
+        
+        ]}
+      >
+        <Meta 
+          title={title} 
+          description={`${description && description.substring(0,40)}...`}
+        />
+      </Card>
+  )
+};
 
 export default ProductCard;
