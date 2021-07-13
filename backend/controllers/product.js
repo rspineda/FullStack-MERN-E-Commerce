57a91e4661aa0abd81e41f4ea86f417f5a836247
Ronald <ronystivens@hotmail.com>
2021-07-13 22:39:56 +0200

    added endpoint for listing new arivals and best sellers on home page!

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -2,60 +2,76 @@ const Product = require('../models/product');
 const slugify = require('slugify');
 
 exports.create = async (req, res ) => {
-    try { 
-        console.log(req.body);
-        req.body.slug = slugify(req.body.title);
-        const newProduct = await new Product(req.body).save();
-        res.json(newProduct);
-    } catch(err){
-        console.log(err);
-        //res.status(400).send("Create product failed");
-        res.status(400).json({
-            err: err.message
-        });
-    }
+  try { 
+    console.log(req.body);
+    req.body.slug = slugify(req.body.title);
+    const newProduct = await new Product(req.body).save();
+    res.json(newProduct);
+  } catch(err){
+    console.log(err);
+    //res.status(400).send("Create product failed");
+    res.status(400).json({
+        err: err.message
+    });
+  }
 };
 
 exports.listAll = async (req, res) => {
-    let list = await Product.find({})
+  let list = await Product.find({})
     .limit(parseInt(req.params.count)) //limit the number of products that are got from backend
     .populate("category")
     .populate("subs")
     .sort([["createdAt", "desc"]])
     .exec();
-    res.json(list);
+  res.json(list);
 };
 
 exports.remove = async (req, res) => {
-    try{
-        const deleted = await Product.findOneAndDelete({slug: req.params.slug}).exec();
-        res.json({
-            "message": "Product deleted successfully",
-            "product_deleted" : deleted
-        })
-    }catch(err){
-        console.log(err);
-        return res.status(400).send('Delete product failed');
-    }
+  try{
+    const deleted = await Product.findOneAndDelete({slug: req.params.slug}).exec();
+    res.json({
+        "message": "Product deleted successfully",
+        "product_deleted" : deleted
+    })
+  }catch(err){
+    console.log(err);
+    return res.status(400).send('Delete product failed');
+  }
 };
 
 exports.read = async (req, res) => {
-    const product = await Product.findOne({slug: req.params.slug})
-      .populate('category')
-      .populate('subs')
-      .exec();
-    res.json(product);
+  const product = await Product.findOne({slug: req.params.slug})
+    .populate('category')
+    .populate('subs')
+    .exec();
+  res.json(product);
 };
 
 exports.update = async (req, res) => {
-    try{
-        const updated = await Product.findOneAndUpdate({slug: req.params.slug}, req.body, {new: true}).exec();
-        res.json(updated)
-    }catch(err){
-        console.log(`error on Udpdate Product: ${err}`);
-        //return res.status(400).send('Update product failed');
-        res.status(400).json({
-            err: err.message,
-        })
-    }
-}
+  try{
+    const updated = await Product.findOneAndUpdate({slug: req.params.slug}, req.body, {new: true}).exec();
+    res.json(updated)
+  }catch(err){
+    console.log(`error on Udpdate Product: ${err}`);
+    //return res.status(400).send('Update product failed');
+    res.status(400).json({
+        err: err.message,
+    })
+  }
+};
+
+exports.list = async (req, res) => {
+  try {
+    // sort: createdAt/updatedAt  order: desc/asc  limit: 3
+    const {sort, order, limit} = req.body;
+    const products = await Product.find({})
+      .populate('category')
+      .populate('subs')
+      .sort([[sort, order]])
+      .limit(limit)
+      .exec();
+      res.json(products);
+  } catch(err) {
+    console.log(err);
+  }
+};
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 //midlewares
-const { authCheck, adminCheck } = require('../middlewares/auth');
+const {authCheck, adminCheck} = require('../middlewares/auth');
 
 //controller
-const { create, read, update, remove, listAll } = require('../controllers/product');
+const {create, read, update, remove, listAll, list} = require('../controllers/product');
 
 //routes
 router.post('/product', authCheck, adminCheck, create);
@@ -13,5 +13,7 @@ router.get('/products/:count', listAll);
 router.delete('/product/:slug', authCheck, adminCheck, remove);
 router.get('/product/:slug', read);
 router.put('/product/:slug', authCheck, adminCheck, update);
+
+router.post('/products', list);
 
 module.exports = router;
