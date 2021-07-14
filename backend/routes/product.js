c4dafefd911bc83687c4ad02997385034f3ad56f
Ronald <ronystivens@hotmail.com>
2021-07-14 16:32:09 +0200

    added endpoint for total number of products on DB

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,15 +5,19 @@ const router = express.Router();
 const {authCheck, adminCheck} = require('../middlewares/auth');
 
 //controller
-const {create, read, update, remove, listAll, list} = require('../controllers/product');
+const {create, read, update, remove, listAll, list, productsCount} = require('../controllers/product');
 
 //routes
 router.post('/product', authCheck, adminCheck, create);
+router.get('/products/total', productsCount);
 router.get('/products/:count', listAll);
 router.delete('/product/:slug', authCheck, adminCheck, remove);
 router.get('/product/:slug', read);
 router.put('/product/:slug', authCheck, adminCheck, update);
 
+//Home
 router.post('/products', list);
+//Pagination
+router.get('/products/total', productsCount);
 
 module.exports = router;
