e6b3a262c6d2ac2241da7bd5b732098873da19a3
Ronald <ronystivens@hotmail.com>
2021-07-14 15:53:46 +0200

    refactored fetching products on home page and moved functions to components folder

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,47 +1,30 @@
-import React, {useEffect, useState} from "react";
-import {getProducts} from '../functions/product';
-import ProductCard from "../components/cards/ProductCard";
+import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
-import LoadingCard from "../components/cards/LoadingCard";
+import NewArrivals from "../components/home/NewArrivals";
+import BestSellers from "../components/home/BestSellers";
 
 const Home = () => {
   
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    loadAllProducts();
-  }, []);
-
-  const loadAllProducts = () => {
-    setLoading(true);
-    getProducts('createdAt', 'desc', 3)
-    .then((res) => {
-      setProducts(res.data);
-      setLoading(false);
-    });
-  };
-
   return (
     <>
-    <div className="jumbotron text-danger h1 font-weight-bold text-center">
-      {/* {loading ? <h3>Loading...</h3> : <h3>All Products</h3>} */}
-      <Jumbotron text={['Latest Products', 'Best Sellers']}/>
-    </div>
-    <div className="container">
-      { loading ? (<LoadingCard count={3}/>) : (
-        <div className="row">
-          {products.map((product) => (
-            <div key={product._id} className="col-md-4">
-              <ProductCard
-                product={product}
-              />
-            </div>
-          ))}
-        </div>
-        )
-      }
-    </div>
+      <div className="jumbotron text-danger h1 font-weight-bold text-center">
+        {/* {loading ? <h3>Loading...</h3> : <h3>All Products</h3>} */}
+        <Jumbotron text={['Latest Products', 'Best Sellers']}/>
+      </div>
+
+      <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+        New Arrivals
+      </h4>
+
+      <NewArrivals/>
+
+      <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+        Best Sellers
+      </h4>
+
+      <BestSellers/>
+      
+      <br/>
     </>
   );
 };
