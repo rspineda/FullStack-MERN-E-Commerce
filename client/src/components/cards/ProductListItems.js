e18ba270c44c1dbb9a812ed7d6ecd6180e26382c
Ronald <ronystivens@hotmail.com>
2021-07-25 11:31:24 +0200

    added more details list when user see the Product Detail

--- client/src/components/cards/ProductListItems.js
+++ client/src/components/cards/ProductListItems.js
@@ -3,33 +3,64 @@ import {Link} from 'react-router-dom';
 
 const ProductListItems = ({product}) => {
 
-  const {price, category} = product;
+  const {
+    price, 
+    category, 
+    subs, 
+    shipping, 
+    color,
+    brand, 
+    quantity, 
+    sold
+  } = product;
 
   return (
     <ul className='list-group'>
+
+      <li className='list-group-item'>
+        Price <span className='label label-default label-pill pull-xs-right'>$ {price}</span>
+      </li>
+
+      {category && (
         <li className='list-group-item'>
-          Price<span className='label label-default label-pill pull-xs-right'>$ {price}</span>
+        Category <Link to={`/category/${category.slug}`} className='label label-default label-pill pull-xs-right'>{category.name}</Link>
         </li>
-        {category && (
-          <li className='list-group-item'>
-          Category<Link to={`/category/${category.slug}`} className='label label-default label-pill pull-xs-right'>$ {category.name}</Link>
-          </li>
-        )}
+      )}
+
+      {subs && (
         <li className='list-group-item'>
-          Price<span className='label label-default label-pill pull-xs-right'>$ {price}</span>
+          Sub Categories {subs.map((s) => (
+            <Link
+              key={s._id}
+              to={`/sub/${s.slug}`}
+              className='label label-default label-pill pull-xs-right'
+            >
+              {s.name}
+            </Link>
+          ))}
         </li>
-        <li className='list-group-item'>
-          Price<span className='label label-default label-pill pull-xs-right'>$ {price}</span>
-        </li>
-        <li className='list-group-item'>
-          Price<span className='label label-default label-pill pull-xs-right'>$ {price}</span>
-        </li>
-        <li className='list-group-item'>
-          Price<span className='label label-default label-pill pull-xs-right'>$ {price}</span>
-        </li>
-        <li className='list-group-item'>
-          Price<span className='label label-default label-pill pull-xs-right'>$ {price}</span>
-        </li>
+      )}
+      
+      <li className='list-group-item'>
+        Shipping <span className='label label-default label-pill pull-xs-right'>{shipping}</span>
+      </li>
+
+      <li className='list-group-item'>
+        Color<span className='label label-default label-pill pull-xs-right'>{color}</span>
+      </li>
+
+      <li className='list-group-item'>
+        Brand<span className='label label-default label-pill pull-xs-right'>{brand}</span>
+      </li>
+
+      <li className='list-group-item'>
+        Available<span className='label label-default label-pill pull-xs-right'>{quantity}</span>
+      </li>
+
+      <li className='list-group-item'>
+        Sold<span className='label label-default label-pill pull-xs-right'>{sold}</span>
+      </li>
+
     </ul>
   );
 };
