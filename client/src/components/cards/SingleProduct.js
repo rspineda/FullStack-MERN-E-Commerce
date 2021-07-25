b437318274755b3839095cc3034454a96d118351
Ronald <ronystivens@hotmail.com>
2021-07-25 23:09:46 +0200

    added more product details on tabs below product pictures

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -1,22 +1,42 @@
 import React from 'react';
-import {Card} from 'antd';
+import {Card, Tabs} from 'antd';
 import {Link} from 'react-router-dom';
 import {HeartOutlined, ShoppingCartOutlined} from '@ant-design/icons';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import Laptop from '../../images/laptop.jpg';
 import ProductListItems from './ProductListItems';
+
+const {TabPane} = Tabs;
 
 const SingleProduct = ({product}) => {
 
-  const {title, images} = product;
+  const {title, images, description} = product;
 
   return (
     <>
       <div className="col-md-7">
-        <Carousel showArrows={true} autoPlay infiniteLoop>
+        {images && images.length ? (
+          <Carousel showArrows={true} autoPlay infiniteLoop>
           {images && images.map((i) => <img src={i.url} key={i.public_id}/>)}
-        </Carousel>
+          </Carousel>
+        ) : (
+          <Card
+            cover={<img src={Laptop} className="mb-3 card-image"/>}
+          >
+          </Card>
+        )}
+        
+        <Tabs type="card">
+          <TabPane tab="Description" key="1">
+            {description && description}
+          </TabPane>
+          <TabPane tab="More" key="2">
+            If you need more information about this product, please call us to +34 666777888
+          </TabPane>
+        </Tabs>
       </div>
+
       <div className="col-md-5">
         <h1 className="bg-info p-3">{title}</h1>
         <Card
