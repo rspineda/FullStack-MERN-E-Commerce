7a472af5e5c71ee3b884049b96090838a1bf5f64
Ronald <ronystivens@hotmail.com>
2021-10-11 23:43:34 +0200

    added average rating to home page

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -3,7 +3,7 @@ import {Card} from 'antd';
 import { EyeOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import laptopImage from '../../images/laptop.jpg';
 import {Link} from 'react-router-dom';
-
+import { showAverage } from '../../functions/rating';
 
 const {Meta} = Card;
 
@@ -12,6 +12,11 @@ const ProductCard = ({product}) => {
   const {images, title, description, slug} = product;
 
   return (
+    <>
+      {product && product.ratings && product.ratings.length > 0 
+        ? showAverage(product) 
+        : <div className="text-center pt-1 pb-3">No rating yet</div>}
+        
       <Card
         cover={
             <img 
@@ -40,6 +45,7 @@ const ProductCard = ({product}) => {
           description={`${description && description.substring(0,40)}...`}
         />
       </Card>
+    </>
   )
 };
 
