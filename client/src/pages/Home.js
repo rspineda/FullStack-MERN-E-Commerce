d932e574a9c707cfb8c80cefaddff167be1f508a
Ronald <ronystivens@hotmail.com>
2021-10-31 21:27:24 +0100

    rendered Categories section on Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
 import NewArrivals from "../components/home/NewArrivals";
 import BestSellers from "../components/home/BestSellers";
+import CategoryList from "../components/category/CategoryList";
 
 const Home = () => {
   
@@ -15,15 +16,17 @@ const Home = () => {
       <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
         New Arrivals
       </h4>
-
       <NewArrivals/>
 
       <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
         Best Sellers
       </h4>
-
       <BestSellers/>
-      
+
+      <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+        Categories
+      </h4>
+      <CategoryList/>    
       <br/>
     </>
   );
